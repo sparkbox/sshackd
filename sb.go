392d96d9a54cdff8b6a5e5e20ea77bc92447793d
Adam Simpson <adam@heysparkbox.com>
2021-05-10 16:10:08 -0400

    feat: basic oauth flow works along with request for users

--- sb.go
+++ sb.go
@@ -1,33 +1,93 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"io"
 	"log"
-
-	"github.com/go-ldap/ldap/v3"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 )
 
-func ExampleConn_WhoAmI() {
-	conn, err := ldap.DialURL("ldap://ldap.forumsys.com:389")
-	if err != nil {
-		log.Fatalf("Failed to connect: %s\n", err)
+type oAuthToken struct {
+	Token string `json:"access_token"`
+	Id    string `json:"user_id"`
+}
+
+type user struct {
+	Id      string
+	IsAdmin bool `json:"is_admin"`
+	Deleted bool
+}
+
+type userList struct {
+	Members []user
+}
+
+func exchangeToken(token string) (d oAuthToken) {
+	var data oAuthToken
+
+	url := url.URL{
+		Scheme:   "https",
+		Host:     "slack.com",
+		Path:     "api/oauth.access",
+		RawQuery: "client_id=" + os.Getenv("CLIENT_ID") + "&client_secret=" + os.Getenv("CLIENT_SECRET") + "&code=" + token + "&redirect_uri=https://sb-app-zvsm8hle.tunnelto.dev",
 	}
 
-	_, err = conn.SimpleBind(&ldap.SimpleBindRequest{
-		Username: "uid=euler,dc=example,dc=com",
-		Password: "password",
-	})
+	res, err := http.Get(url.String())
+
 	if err != nil {
-		log.Fatalf("Failed to bind: %s\n", err)
+		log.Fatal("failed to request oauth token: ", err)
 	}
 
-	res, err := conn.WhoAmI(nil)
-	if err != nil {
-		log.Fatalf("Failed to call WhoAmI(): %s\n", err)
+	json.NewDecoder(res.Body).Decode(&data)
+
+	return data
+}
+
+func get_info(access_token string) (data userList) {
+	url := url.URL{
+		Scheme: "https",
+		Host:   "slack.com",
+		Path:   "api/users.list",
 	}
-	fmt.Printf("I am: %s\n", res.AuthzID)
+
+	client := &http.Client{}
+
+	request, err := http.NewRequest("GET", url.String(), nil)
+
+	if err != nil {
+		log.Fatal("failed to create new request: ", err)
+	}
+
+	request.Header.Set("authorization", "Bearer "+access_token)
+
+	res, err := client.Do(request)
+
+	if err != nil {
+		log.Fatal("failed to do request: ", err)
+	}
+
+	json.NewDecoder(res.Body).Decode(&data)
+
+	defer res.Body.Close()
+
+	if err != nil {
+		log.Fatal("failed to parse user list: ", err)
+	}
+
+	return data
 }
 
 func main() {
-	ExampleConn_WhoAmI()
+	root := func(w http.ResponseWriter, req *http.Request) {
+		tempCode := req.URL.Query()["code"]
+		data := exchangeToken(strings.Join(tempCode, ""))
+		_ = get_info(data.Token)
+		io.WriteString(w, "transferring...\n")
+	}
+
+	http.HandleFunc("/", root)
+	log.Fatal(http.ListenAndServe(":9001", nil))
 }
