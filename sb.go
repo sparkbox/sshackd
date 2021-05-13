88e70ffb205a0b333afcdf5cf62781f524e0f076
Adam Simpson <adam@heysparkbox.com>
2021-05-13 09:39:22 -0400

    feat: /ssh route properly validates users

--- sb.go
+++ sb.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"sb/slack"
 	"strings"
 )
+
+type SSHLogin struct {
+	Token string
+}
 
 func filterUsers(users []slack.User) (filteredUsers []slack.User) {
 	for i := 0; i < len(users); i++ {
 		emailDomain := returnDomain(users[i].Profile.Email)
 		deleted := users[i].Deleted
-
 		if emailDomain == "heysparkbox.com" && !deleted {
 			filteredUsers = append(filteredUsers, users[i])
 		}
@@ -43,18 +48,32 @@ func login(w http.ResponseWriter, req *http.Request) {
 
 	if code != "" {
 		data := slack.ExchangeToken(code)
-		users := slack.UsersList(data.Authed_user.Access_token)
-		filteredUsers := filterUsers(users)
-		isValid := isValidUser(filteredUsers, data.Authed_user.Id)
-		if isValid {
-			io.WriteString(w, data.Authed_user.Id+":"+data.Authed_user.Access_token)
-		} else {
-			w.WriteHeader(401)
-		}
+		io.WriteString(w, data.Authed_user.Id+":"+data.Authed_user.Access_token)
+	}
+}
+
+func ssh(w http.ResponseWriter, req *http.Request) {
+	var s SSHLogin
+
+	json.NewDecoder(req.Body).Decode(&s)
+
+	defer req.Body.Close()
+
+	splits := strings.Split(s.Token, ":")
+
+	users := slack.UsersList(splits[1])
+	filteredUsers := filterUsers(users)
+	isValid := isValidUser(filteredUsers, splits[0])
+	if isValid {
+		//sign key
+		io.WriteString(w, "signing key!")
+	} else {
+		w.WriteHeader(401)
 	}
 }
 
 func main() {
 	http.HandleFunc("/login", login)
-	log.Fatal(http.ListenAndServe(":9001", nil))
+	http.HandleFunc("/ssh", ssh)
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
